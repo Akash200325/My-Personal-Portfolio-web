252a50d424cc18762be03a2b1edb06c01c07d4bf
Akash200325 <142168006+Akash200325@users.noreply.github.com>
2024-11-26 23:53:33 +0530

    Delete my-portfolio-main directory

--- my-portfolio-main/main.js
+++
@@ -1,6 +0,0 @@
-const projectLinks = document.querySelectorAll('.project-card a');
-projectLinks.forEach(link => {
-    link.addEventListener('click', () => {
-        alert('Redirecting to the project link!');
-    });
-});
